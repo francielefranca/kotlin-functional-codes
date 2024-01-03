5dd1e0e42bf894f40033b3cfed2f176b64294653
Franciele França <92552849+francielefranca@users.noreply.github.com>
2024-01-03 18:54:18 -0300

    Create if-ellse.kt

---
+++ if-ellse.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println("Hello, world!!!")
+    
+    println()
+    
+    println("Digite seu nome e senha:")
+    var nomeu = "Hole"
+    var senhau = "abacate"
+    
+    println(nomeu)
+    println(senhau)
+    
+    var nome = "Hole"
+    var senha = "abacate"
+    
+    
+    if(nome == nomeu && senha == senhau){
+        println("Bem vindo(a), $nome !")
+    }else{
+        println("Um dos dados não coincidem.")
+    }
+}
